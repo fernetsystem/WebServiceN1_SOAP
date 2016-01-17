a2bc5578b7296135fd301f9a1bda051aed2d6601
Jth Rdz <fernetsystem@gmail.com>
2016-01-16 23:37:23 -0600

    cambiar dominio

--- WebServiceN1_SOAP/WebService1.asmx.cs
+++ WebServiceN1_SOAP/WebService1.asmx.cs
@@ -9,7 +9,7 @@ namespace WebServiceN1_SOAP
     /// <summary>
     /// Summary description for WebService1
     /// </summary>
-    [WebService(Namespace = "http://tempuri.org/")]
+    [WebService(Namespace = "http://elity.com/service/serv01")]
     [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
     [System.ComponentModel.ToolboxItem(false)]
     // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
